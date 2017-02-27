ae8e6284cf0e127af890a978c67b9ac98e26b0db
Unknown <MOTYSHIZ@gmail.com>
2017-02-26 17:42:59 -0800

    Started Project 2 and did basic file input and table printing.

---
+++ newton-polynomial-interpolator/src/Main.java
@@ -0,0 +1,55 @@
+/**
+ * Created by Justin Ordonez on 2/26/2017.
+ */
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    private static float[] xArray;
+    private static float[] yArray;
+
+    public static void main(String args[]){
+        File file = new File("input.txt");
+
+        try {
+            Scanner sc = new Scanner(file);
+
+            String[] xStringarray = sc.nextLine().split(" ");
+            String[] yStringarray = sc.nextLine().split(" ");
+            xArray = new float[xStringarray.length];
+            yArray = new float[yStringarray.length];
+
+            for(int i = 0; i < xStringarray.length; i++){
+                xArray[i] = Float.parseFloat(xStringarray[i]);
+                yArray[i] = Float.parseFloat(yStringarray[i]);
+            }
+            sc.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for(int i = 0; i < xArray.length; i++){
+            System.out.print(xArray[i] + " ");
+        } System.out.println();
+
+        for(int i = 0; i < yArray.length; i++){
+            System.out.print(yArray[i] + " ");
+        }System.out.println();
+
+        dividedDifferenceTablePrinter();
+    }
+
+    private static void dividedDifferenceTablePrinter(){
+        String[] topLabel = {"x", "f[]", "f[,]", "f[,,]", "f[,,,]"};
+
+        for(int i = 0; i < topLabel.length; i++){
+            System.out.printf("%-7s ", topLabel[i]);
+        }System.out.println();
+
+        for(int i = 0; i < xArray.length; i++){
+            System.out.printf("%-7.3f %-7.3f\n", xArray[i], yArray[i]);
+        }
+    }
+}
